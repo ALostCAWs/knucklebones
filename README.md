e2d45bbc58800ef3603268b0faa7d31f8a08ae48
ALostCAWs <keighly@keighly.ca>
2024-04-26 21:38:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing a layout concept that I have not attempted before
---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="page-wrapper">
+      <h1 class="hiddenHeading">Knucklebones</h1>
+      <div class="game-container">
+        <div class="throw-container">
+          <button></button>
+        </div>
+        <div class="dice-container">
+          <button v-for="index in 9"></button>
+        </div>
+        <div class="throw-container">
+          <button></button>
+        </div>
+        <div class="dice-container">
+          <button v-for="index in 9"></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+    };
+  }
+}
+</script>
+
+<style>
+@import url('./styles/styles.css');
+h1.hiddenHeading {
+  position: absolute;
+  padding: 0;
+  width: 1px;
+  height: 1px;
+  border: 0;
+  clip: rect(1px, 1px, 1px, 1px);
+  -webkit-clip-path: inset(0px 0px 99.9% 99.9%);
+  clip-path: inset(0px 0px 99.9% 99.9%);
+}
+
+h2:not(aside h2) {
+  padding-bottom: 1em;
+}
+
+button {
+  background-color: var(--accent-color-faded);
+}
+
+button:not(aside button) {
+  width: 5em;
+  height: 1.5em;
+  border: thin solid var(--accent-color);
+  border-radius: 5px;
+
+  color: var(--accent-color);
+}
+
+button:hover {
+  background-color: var(--accent-color);
+}
+
+button:not(aside button):hover {
+  border: thin solid var(--light-color);
+}
+
+button:hover {
+  color: var(--accent-color-faded);
+}
+
+.game-container {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 50% 50%;
+
+  width: 90%;
+  height: 70vh;
+}
+
+.dice-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+.game-container > .dice-container:not(:nth-child(2)) {
+  margin-top: 2em;
+}
+
+.dice-container button {
+  margin: auto;
+}
+
+.throw-container {
+  margin: auto;
+}
+</style>
